7fc8cb929a7672674c30894d7adf6082a6bd7814
tu2id4n <84172259@qq.com>
2019-11-19 16:26:15 +0800

    add pretrain

--- my_common/__init__.py
+++ my_common/__init__.py
@@ -3,3 +3,4 @@ from .cmd_utils import arg_parser
 from .feature_utils import get_observertion_space, get_action_space, get_bomb_life, featurize, get_prev2obs, get_modify_act
 from .prune import get_filtered_actions
 from .subproc_vec_env import SubprocVecEnv
+from .dataset import ExpertDataset
---
+++ pretrain.py
@@ -0,0 +1,72 @@
+import pommerman
+from pommerman import agents
+import sys
+import os
+import multiprocessing
+import tensorflow as tf
+from my_common.cmd_utils import arg_parser
+from my_common.subproc_vec_env import SubprocVecEnv
+from my_policies import PGNPolicy, ResNetPolicy
+from my_baselines import PPO2
+from my_common import ExpertDataset
+
+def make_envs(env_id):
+    def _thunk():
+        agent_list = [
+            agents.SimpleAgent(),
+            agents.SimpleAgent(),
+            agents.SimpleAgent(),
+            agents.SimpleAgent()
+        ]
+        env = pommerman.make(env_id, agent_list)
+        return env
+
+    return _thunk
+
+
+def get_my_policy():
+    if args.policy_type == 'resnet':
+        return ResNetPolicy
+    elif args.policy_type == 'pgn':
+        return PGNPolicy
+
+
+def _pretrain():
+    # Mutiprocessing
+    config = tf.ConfigProto()
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    config.gpu_options.allow_growth = True
+    num_envs = args.num_env or multiprocessing.cpu_count()
+    envs = [make_envs(args.env) for _ in range(num_envs)]
+    env = SubprocVecEnv(envs)
+
+    num_episodes = int(args.num_timesteps)
+    if args.load_path:
+        print()
+        print("Load a model from", args.load_path)
+        print()
+        model = PPO2.load(args.load_path, env=env, using_pgn=args.using_pgn, tensorboard_log=args.log_path,
+                          policy=get_my_policy())
+    else:
+        model = PPO2(get_my_policy(), env=env, verbose=1, tensorboard_log=args.log_path)
+
+    print()
+    print("**************** Start to pretrain ****************")
+    print("num_episodes = ", num_episodes)
+    print("policy_type = ", args.policy_type)
+    print("env = ", args.env)
+    print("num_envs = ", num_envs)
+    print("expert_dir_path = ", args.expert_path)
+    print("save_path = ", args.save_path)
+    print()
+    expert_dataset = ExpertDataset(expert_path=args.expert_path)
+    model.pretrain(dataset=expert_dataset, n_epochs=num_episodes, save_path=args.save_path)
+
+    env.close()
+
+
+
+if __name__ == '__main__':
+    arg_parser = arg_parser()
+    args, unknown_args = arg_parser.parse_known_args(sys.argv)
+    _pretrain()
